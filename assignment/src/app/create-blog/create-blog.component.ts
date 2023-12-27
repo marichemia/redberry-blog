6513b31412687c2157222a55782793ae362fbcfb
mari chemia <marichemia10@gmail.com>
2023-12-28 01:36:38 +0400

    finished create blog form html&css

--- assignment/src/app/create-blog/create-blog.component.ts
+++ assignment/src/app/create-blog/create-blog.component.ts
@@ -1,15 +1,27 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CategoriesService } from '../core/services/categories.service';
+import { Category } from '../core/interfaces/category';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create-blog',
   templateUrl: './create-blog.component.html',
   styleUrls: ['./create-blog.component.scss']
 })
-export class CreateBlogComponent {
+export class CreateBlogComponent implements OnInit {
 
   selectedFile: File | null | undefined;
   fileName: string | null | undefined;
+  categoriesArr$: Observable<Category[]> | undefined;
   @ViewChild('fileInput') fileInput: ElementRef | undefined;
+
+  constructor(private categoriesService: CategoriesService) { }
+
+  ngOnInit() {
+
+    this.categoriesArr$ = this.categoriesService.getCategories();
+
+  }
 
   onFileSelected(event: any) {
     this.selectedFile = <File>event.target?.files[0];
